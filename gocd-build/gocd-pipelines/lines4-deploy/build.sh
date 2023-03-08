6cb24210c186d13055ae3569b1c9a24ce1eed3aa
nickolit <kbaht2006@gmail.com>
2023-03-08 12:59:29 +0300

    AppBuild

--- gocd-build/gocd-pipelines/lines4-deploy/build.sh
+++ gocd-build/gocd-pipelines/lines4-deploy/build.sh
@@ -5,11 +5,11 @@ docker build -t lines-four .
 echo "Let us pretend that this is building something ... and takes 20 seconds"
 
 count=0
-while [ "$count" -lt 20 ]; do
+while [ "$count" -lt 10 ]; do
     count=$((count + 1))
 
     color_code=$((31 + (count % 7)))
-    printf "\e[01;${color_code}mBuilding [$count of 20]\e[00m\n"
+    printf "\e[01;${color_code}mBuilding [$count of 10]\e[00m\n"
 
     sleep 1
 done
